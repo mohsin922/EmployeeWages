fc688552060358bb384e0658889dbe4d4a299d6e
unknown <mohsinpeer22@gmail.com>
2021-12-15 00:24:58 +0530

    Pushing Employee Present or Absent(UC1)

---
+++ EmployeeWages/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random(); //function while random is class
+            int empCheck = random.Next(0, 2); //Next is method in random class
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+
+
+        }
+    }
+}
