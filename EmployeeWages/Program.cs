9589536521e345c629b09d07cce16754d9ab509d
unknown <mohsinpeer22@gmail.com>
2021-12-15 00:33:58 +0530

    Pushing Employee Daily Wage(UC2)

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -6,18 +6,27 @@ namespace Employee
     {
         static void Main(string[] args)
         {
+            //Constants
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random(); //function while random is class
+            //Computation
             int empCheck = random.Next(0, 2); //Next is method in random class
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is present");
+                empHrs = 8;
 
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
+
 
 
         }
