fe7937fd46ab86ebf4bd5f8cf782bb38b33e7b62
unknown <mohsinpeer22@gmail.com>
2021-12-15 11:56:52 +0530

    Pushing Part time Employee Assignment(UC3)

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -8,17 +8,22 @@ namespace Employee
         {
             //Constants
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             //Variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random(); //function while random is class
             //Computation
-            int empCheck = random.Next(0, 2); //Next is method in random class
+            int empCheck = random.Next(0, 3); //Next is method in random class
             if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
 
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
             }
             else
             {
