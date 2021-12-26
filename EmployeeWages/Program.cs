6187afdeceea50814853333fafc2a09c6f7b8a4f
unknown <mohsinpeer22@gmail.com>
2021-12-26 13:05:04 +0530

    Manage Employee Wage of Multiple Companies

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -3,29 +3,40 @@
 namespace Employee
 {
 
-    public class EmpWageBuilderObject
+    public class EmpWageBuilderArray
     {
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
 
-        private string company;
-        private int empRatePerHour;
-        private int numOfWorkingDays;
-        private int maxHoursPerMonth;
-        private int totalEmpWage;
+        private int numOfCompany = 0;
+        private CompanyEmpWage[] companyEmpWageArray;
 
-        public EmpWageBuilderObject(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        public EmpWageBuilderArray()
         {
-            this.company = company;
-            this.empRatePerHour = empRatePerHour;
-            this.numOfWorkingDays = numOfWorkingDays;
-            this.maxHoursPerMonth = maxHoursPerMonth;
+            this.companyEmpWageArray = new CompanyEmpWage[5];
+        }
+
+        public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            companyEmpWageArray[this.numOfCompany] = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+            numOfCompany++;
+        }
+
+        
+
+        public void computeEmpWage()
+        {
+            for (int i = 0; i < numOfCompany; i++)
+            {
+                companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(this.companyEmpWageArray[i]));
+                Console.WriteLine(this.companyEmpWageArray[i].toString());
+            }
         }
 
 
 
 
-        public void computeEmpWage()
+        private int computeEmpWage(CompanyEmpWage companyEmpWage)
         {
 
             //Variables
@@ -36,7 +47,7 @@ namespace Employee
 
             int totalEmpHrs = 0;
 
-            while (totalWorkingDays <= this.numOfWorkingDays && totalEmpHrs <= this.maxHoursPerMonth)
+            while (totalWorkingDays < companyEmpWage.numOfWorkingDays && totalEmpHrs <= companyEmpWage.maxHoursPerMonth)
             {
                 totalWorkingDays++;
                 Random random = new Random(); //function while random is class
@@ -59,30 +70,46 @@ namespace Employee
                 Console.WriteLine("Day#: " + totalWorkingDays + " EMP  hours :" + empHrs);
 
             }
-            totalEmpWage = totalEmpHrs * empRatePerHour;
-            Console.WriteLine("Total emp Wage for company : " + company + " is:" + totalEmpWage);
+            return totalEmpHrs * companyEmpWage.empRatePerHour;
+            
         }
+    }
+    public class CompanyEmpWage
+    {
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+
+        public CompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+
+
         public string toString()
         {
             return "Total emp Wage for company : " + this.company + " is:" + this.totalEmpWage;
         }
-
-
-
-
+   
     }
     class program
     {
 
         static void Main(string[] args)
         {
-            EmpWageBuilderObject dMart = new EmpWageBuilderObject("Dmart", 20, 2, 10);
-            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 20, 2, 10);
-            dMart.computeEmpWage();
-            Console.WriteLine(dMart.toString());
-            reliance.computeEmpWage();
-            Console.WriteLine(reliance.toString());
-
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 20, 2, 10);
+            empWageBuilder.computeEmpWage();
 
         }
     }
